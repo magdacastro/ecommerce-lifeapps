a7136764d550db8855182edb12813fa2e6b1f101
Magda <magdacatianedecastro@gmail.com>
2024-04-27 15:12:35 -0300

    initial home implementation

---
+++ components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav class="navbar bg-body-tertiary">
+    <div class="container-fluid">
+      <div class="row" style="width: 100vw">
+        <a class="navbar-brand col-lg-4 m-0" href="#">
+          <img src="/assets/images/ecommerce.svg" class="w-size" alt="Logo" />
+        </a>
+        <div class="d-flex align-items-center col-lg-8" role="search">
+          <div class="input-group rounded me-4">
+            <input
+              type="search"
+              class="form-control border-end-0"
+              placeholder="Quero comprar algo específico..."
+              aria-label="Search"
+            />
+            <span class="input-group-text border-end-1" id="search-addon">
+              <i class="fas fa-search"></i>
+            </span>
+          </div>
+          <a class="me-4"><img src="/assets/images/profile_img.svg" alt="Profile" /></a>
+          <a class="me-4"><img src="/assets/images/shopping_img.svg" alt="Shopping" /></a>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,39 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+  app: {
+    head: {
+      script: [
+        {
+          src: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js",
+          integrity:
+            "sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz",
+          crossorigin: "anonymous",
+        },
+        {
+          src: "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js",
+          integrity:
+            "sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r",
+          crossorigin: "anonymous",
+        },
+        {
+          src: "https://kit.fontawesome.com/c97a6b65ef.js",
+          crossorigin: "anonymous"
+        }
+      ],
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css",
+          integrity:
+            "sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH",
+          crossorigin: "anonymous",
+        },
+        {
+          rel: "stylesheet",
+          href: "_nuxt/assets/css/index.css"
+        }
+      ],
+    },
+  },
+});
